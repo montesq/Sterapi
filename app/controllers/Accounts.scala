3b9d5ab129551b6de466f6e000425a7476b4b401
montesq <montesq@aliceadsl.fr>
2013-05-19 23:35:13 +0200

    Remove useless import

--- app/controllers/Accounts.scala
+++ app/controllers/Accounts.scala
@@ -1,32 +1,32 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
-import play.api.Play.current
 import play.api.libs.json._
-import play.api.libs.iteratee._
-import scala.concurrent.{ ExecutionContext, Future }
 import play.modules.reactivemongo._
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.api._
 import models.Accounts._
 
-object Accounts extends Controller with MongoController{
+object Accounts extends Controller with MongoController {
 
   val collection = db.collection[JSONCollection]("accounts")
 
-  def createAccount = Action(parse.json) { request =>
-    request.body.transform(validateAccount).map{ json =>
-      Async{
-        collection.insert(json).map{lastError =>
-          Created
-        }.recover{ case e =>
-          BadRequest(JsString("exception %s".format(e.getMessage)))
-        }
+  def createAccount = Action(parse.json) {
+    request =>
+      request.body.transform(validateAccount).map {
+        json =>
+          Async {
+            collection.insert(json).map {
+              lastError =>
+                Created
+            }.recover {
+              case e =>
+                BadRequest(JsString("exception %s".format(e.getMessage)))
+            }
+          }
+      }.recoverTotal {
+        err =>
+          BadRequest(JsError.toFlatJson(err))
       }
-    }.recoverTotal{ err =>
-      BadRequest(JsError.toFlatJson(err))
-    }
   }
 
   def listAccounts = TODO
