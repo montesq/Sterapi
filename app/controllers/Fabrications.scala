a700b1c7b133db3b781a3249e7544c01b06965d0
montesq <montesq@aliceadsl.fr>
2013-08-11 00:10:39 +0200

    Generate a specific fabrication _id

--- app/controllers/Fabrications.scala
+++ app/controllers/Fabrications.scala
@@ -12,20 +12,47 @@ import jsonFormaters.AccountsFormaters._
 import play.modules.reactivemongo.json.collection.JSONCollection
 import jsonFormaters.CommonFormaters._
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.core.commands.GetLastError
+import reactivemongo.core.commands.{Update, FindAndModify, GetLastError}
 import play.modules.reactivemongo.json.collection.JSONCollection
-import reactivemongo.core.commands.GetLastError
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
-import reactivemongo.bson.BSONObjectID
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
 import play.api.mvc._
 import java.io.File
 import jsonFormaters.FabricationFormaters._
 import scala.concurrent.Future
+import org.joda.time.DateTime
 
 object Fabrications extends Controller with MongoController with DeadboltActions{
   val fabCollection = DBConnection.db.collection[JSONCollection]("fabrications")
   val accountCollection = DBConnection.db.collection[JSONCollection]("accounts")
+  val seqCollection = DBConnection.db.collection[JSONCollection]("sequences")
+
+  def generateFabId: Future[JsObject] = {
+    val currentYear = ((new DateTime).getYear() % 100)
+    val futureId: Future[Option[JsObject]] = seqCollection.find(Json.obj(
+      "_id" -> "fabrications",
+      "year" -> currentYear)).one[JsObject]
+
+    futureId.map { result => {
+      seqCollection.update(
+        Json.obj(
+        "_id" -> "fabrications",
+        "year" -> currentYear),
+        Json.obj("$inc" -> Json.obj("lastValue" -> 1)),
+        upsert = true
+      ).value
+        result match {
+          case None => Json.obj("_id" -> JsString(currentYear + "0001"))
+          case Some(jsObj) => {
+            val seqVal = (jsObj \ "lastValue").asOpt[Int].get
+            Json.obj("_id" -> JsString(currentYear + "%04d".format(seqVal)))
+          }
+        }
+      }
+    }
+  }
+
 
   def create = //Restrict(Array("MANAGE_FABRICATIONS"), new SecurityHandler) {
     CORSAction {
@@ -36,13 +63,20 @@ object Fabrications extends Controller with MongoController with DeadboltActions
               jsObj.transform((__ \ "_id" \ "$oid").json.copyFrom(( __ \ "client" \ "_id").json.pick)).get
             ).one[JsObject].map {
               case Some(clientObj) => Async {
-                  fabCollection.insert(
-                    jsObj ++ Json.obj("client" -> clientObj.transform(outputAccount).get),
-                    GetLastError(true)).map { lastError =>
-                    if (lastError.ok)
-                      Created
-                    else InternalServerError(JsString("exception %s".format(lastError.errMsg)))
+                generateFabId.map { jsId =>
+                  Async {
+                    fabCollection.insert(
+                      jsObj ++
+                        jsId ++
+                        Json.obj("client" -> clientObj.transform(outputAccount).get),
+                      GetLastError(true)
+                    ).map { lastError =>
+                      if (lastError.ok)
+                        Created
+                      else InternalServerError("erreur")
+                    }
                   }
+                }
               }
               case None => BadRequest(Json.obj("err" -> "Incoherent Client ID"))
             }
@@ -63,7 +97,7 @@ object Fabrications extends Controller with MongoController with DeadboltActions
             .cursor[JsObject]
             .toList
           fabFutureList.map { list =>
-            val transformedList = for (fabrication <- list) yield fabrication.transform(removeOid).get
+            val transformedList = for (fabrication <- list) yield fabrication
             Ok(JsArray(transformedList))
           }.recover { case e =>
             InternalServerError(JsString("exception %s".format(e.getMessage)))
@@ -122,26 +156,6 @@ object Fabrications extends Controller with MongoController with DeadboltActions
     }
   //  }
 
-//  def updateOne(id: String) = //Restrict(Array("MANAGE_FABRICATIONS"), new SecurityHandler) {
-//    CORSAction {
-//      Action(parse.json) { json =>
-//        json.body.transform(validateFabrication andThen toUpdate).map { updateObj =>
-//          Async {
-//            fabCollection.update(
-//              Json.obj("_id" -> Json.obj("$oid" -> id)),
-//              updateObj
-//            ).map { lastError =>
-//              if (lastError.ok)
-//                Ok(updateObj)
-//              else
-//                InternalServerError(JsString("exception %s".format(lastError.errMsg)))
-//            }
-//          }
-//        }.getOrElse(InternalServerError)
-//      }
-//    }
-//  //  }
-//
   def saveAttachment(idFab: String) = CORSAction {
     Action(parse.temporaryFile) { request =>
       request.getQueryString("name") match {
